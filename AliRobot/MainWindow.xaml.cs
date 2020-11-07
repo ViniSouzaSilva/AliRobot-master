923c676c9211091f305f66126429a8ad86be053c
vinicius souza <vinicius291996@hotmail.com>
2020-11-07 00:15:44 -0300

    robo integrado com excel

--- AliRobot/MainWindow.xaml.cs
+++ AliRobot/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using ExcelDataReader;
+﻿using ClosedXML.Excel;
+using ExcelDataReader;
+using Microsoft.Win32;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
@@ -6,7 +8,9 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -28,24 +32,35 @@ namespace AliRobot
         public MainWindow()
         {
             InitializeComponent();
-            PesquisaURl();
+            ExcelReader();
+            
         }
 
 
-        IWebDriver driver;
-
-        public void PesquisaURl()
+        //IWebDriver driver;
+        ChromeDriver driver = new ChromeDriver();
+        public void PesquisaURl(string Url)
         {
-            ChromeDriver driver = new ChromeDriver();
-            string Url = "https://pt.aliexpress.com/item/4001119810724.html?spm=a2g0o.productlist.0.0.5ab96c2awRdZIz&s=p&ad_pvid=202009031053563599469359044750002867920_1&algo_pvid=1ab80081-792b-49b5-87b4-31524bacf481&algo_expid=1ab80081-792b-49b5-87b4-31524bacf481-4&btsid=0ab50f4415991556361922742ea5fd&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_";
+            
+           // string Url = "https://pt.aliexpress.com/item/4001119810724.html?spm=a2g0o.productlist.0.0.5ab96c2awRdZIz&s=p&ad_pvid=202009031053563599469359044750002867920_1&algo_pvid=1ab80081-792b-49b5-87b4-31524bacf481&algo_expid=1ab80081-792b-49b5-87b4-31524bacf481-4&btsid=0ab50f4415991556361922742ea5fd&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_";
             try
             {
                 driver.Navigate().GoToUrl(Url);
-                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
-                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+                Thread.Sleep(7000);
                 wait.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(By.XPath("//*[@id='root']/div/div[2]/div/div[2]/div[1]/h1")));
-                driver.FindElement(By.XPath("//*[@id='root']/div/div[2]/div/div[2]/div[3]/div"));
-                audit("PRODUTO INATIVO",Url);
+               var b =  driver.FindElement(By.XPath("//*[@id='root']/div/div[2]/div/div[2]/div[3]/div")).Text;
+                if (b.Equals("Lamentamos mas este produto já não está disponível") || b.Contains("Indisponível"))
+                {
+                    audit("PRODUTO INATIVO", Url);
+                }
+                else 
+                {
+                    var a = driver.FindElement(By.XPath("//*[@id='root']/div/div[2]/div/div[2]/div[4]/div[1]/span")).Text;
+                    audit(a + "   -", Url);
+                }
+                
 
             }
             catch (Exception)
@@ -53,11 +68,12 @@ namespace AliRobot
                 try
                 {
                     var a = driver.FindElement(By.XPath("//*[@id='root']/div/div[2]/div/div[2]/div[4]/div[1]/span")).Text;
+                    audit(a +"   -", Url);
                 }
-                catch (Exception)
+                catch (Exception ex)
                 {
-
-                    throw;
+                    audit("Erro de consulta \n" + ex.Message, "\n" + Url);
+                   // throw;
                 }
                 
             }
@@ -81,35 +97,53 @@ namespace AliRobot
             }
         }//Escreve no arquivo da auditoria.
 
-        /*
-        public void ExcelReader() 
+
+        public void ExcelReader()
         {
-             
-            using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read))
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
             {
-                // Auto-detect format, supports:
-                //  - Binary Excel files (2.0-2003 format; *.xls)
-                //  - OpenXml Excel files (2007 format; *.xlsx, *.xlsb)
-                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                var PlanCaminho = new XLWorkbook(openFileDialog.FileName);
+                var planinha = PlanCaminho.Worksheet(1);
+
+                var linha = 2;
+                while (true) 
                 {
-                    // Choose one of either 1 or 2:
 
-                    // 1. Use the reader methods
-                    do
-                    {
-                        while (reader.Read())
-                        {
-                            // reader.GetDouble(0);
-                        }
-                    } while (reader.NextResult());
-
-                    // 2. Use the AsDataSet extension method
-                    //var result = reader.AsDataSet();
-
-                    // The result of each spreadsheet is in result.Tables
+                    var nome = planinha.Cell("A" + linha.ToString()).Value.ToString();
+                    if (string.IsNullOrEmpty(nome)) break;
+                    PesquisaURl(nome);
+                    linha++;
                 }
-            }*/
+                PlanCaminho.Dispose();
+               
+            }
+            /*  
+             using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read))
+             {
+                 // Auto-detect format, supports:
+                 //  - Binary Excel files (2.0-2003 format; *.xls)
+                 //  - OpenXml Excel files (2007 format; *.xlsx, *.xlsb)
+                 using (var reader = ExcelReaderFactory.CreateReader(stream))
+                 {
+                     // Choose one of either 1 or 2:
 
+                     // 1. Use the reader methods
+                     do
+                     {
+                         while (reader.Read())
+                         {
+                             // reader.GetDouble(0);
+                         }
+                     } while (reader.NextResult());
+
+                     // 2. Use the AsDataSet extension method
+                     //var result = reader.AsDataSet();
+
+                     // The result of each spreadsheet is in result.Tables
+                 }
+             }*/
+        }
     }
 }
 
